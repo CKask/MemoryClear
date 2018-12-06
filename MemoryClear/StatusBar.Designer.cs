600b4e02f28cff6b07a4a5992463d7f7677e2c95
kg <kgaskt@gmail.com>
2018-12-06 17:31:41 +0800

    更改

--- MemoryClear/StatusBar.Designer.cs
+++ MemoryClear/StatusBar.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MemoryClear
 {
-    partial class StatusBar
+    partial class Status
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.contextMenuStripMouseRight = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.显示主界面ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStripMouseRight.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // StatusBar
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.Color.Transparent;
+            this.lblMessage.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblMessage.ForeColor = System.Drawing.Color.Green;
+            this.lblMessage.Location = new System.Drawing.Point(12, 20);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(37, 20);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "dfdf";
+            this.lblMessage.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lblMessage_MouseDoubleClick);
+            this.lblMessage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblMessage_MouseDown);
+            this.lblMessage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblMessage_MouseMove);
+            // 
+            // contextMenuStripMouseRight
+            // 
+            this.contextMenuStripMouseRight.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示主界面ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStripMouseRight.Name = "contextMenuStripMouseRight";
+            this.contextMenuStripMouseRight.Size = new System.Drawing.Size(137, 48);
+            // 
+            // 显示主界面ToolStripMenuItem
+            // 
+            this.显示主界面ToolStripMenuItem.Name = "显示主界面ToolStripMenuItem";
+            this.显示主界面ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.显示主界面ToolStripMenuItem.Text = "显示主界面";
+            this.显示主界面ToolStripMenuItem.Click += new System.EventHandler(this.显示主界面ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MemoryClear.Properties.Resources._902397dda144ad34478efb09d6a20cf431ad850e__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(-81, -50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Status
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Name = "StatusBar";
-            this.Text = "StatusBar";
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(60, 60);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Status";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Status";
             this.TopMost = true;
-            this.Load += new System.EventHandler(this.StatusBar_Load);
+            this.Load += new System.EventHandler(this.Status_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Status_Paint);
+            this.Resize += new System.EventHandler(this.Status_Resize);
+            this.contextMenuStripMouseRight.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStripMouseRight;
+        private System.Windows.Forms.ToolStripMenuItem 显示主界面ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
